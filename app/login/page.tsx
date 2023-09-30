d610edcf9bb291d5d1bebf4e4db64892fbc45dc8
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-09-30 14:21:38 +0200

    Remove some default code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,20 +8,6 @@ export default function Login() {
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
         Back
       </Link>
 
@@ -54,7 +40,7 @@ export default function Login() {
         </button>
         <button
           formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
+          className="border border-gray-700 rounded px-4 py-2 text-white mb-2"
         >
           Sign Up
         </button>
