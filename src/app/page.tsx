d1b810e816adc6d0ffdf1693851ab699d0a00339
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2023-10-01 08:19:00 +0200

    Fix dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
 import {cookies} from 'next/headers'
-import Link from 'next/link'
-import LogoutButton from '../components/LogoutButton'
+
+export const dynamic = 'force-dynamic'
+
 
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies })
